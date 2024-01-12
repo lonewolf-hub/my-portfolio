bba63240ed799ad52937cb3ef982ab3a15084dba
Jai Shukla <iamshuklajai@gmail.com>
2024-01-13 01:59:52 +0530

    minor changes

--- src/app/core/components/home/HomeComponent.tsx
+++ src/app/core/components/home/HomeComponent.tsx
@@ -1,67 +1,74 @@
 import React from 'react'
-import {  AiFillGithub,AiOutlineTwitter,AiOutlineMail} from 'react-icons/ai'
-import {RiLinkedinFill} from 'react-icons/ri'
+import { AiFillGithub, AiOutlineTwitter, AiOutlineMail } from 'react-icons/ai'
+import { RiLinkedinFill } from 'react-icons/ri'
 
 const HomeComponent = () => {
   return (
     <div id='home' className=' w-full h-screen text-center bg-bodyBackground'>
-    <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-    <p className='uppercase text-sm tracking-widest text-textColor'> let's build something together</p>
-    
-    <h1 className='py-4 text-textColor'> Hey, I'm <span className= 'text-[#5651e5] '>Jai Shukla</span> </h1>
+          <p className='uppercase text-sm tracking-widest text-textColor'> let's build something together</p>
 
-    <h1 className='py-25 uppercase text-[200%] text-textColor'> A Full-Stack Web Developer</h1>
+          <h1 className='py-4 text-textColor'> Hey, I'm <span className='text-[#5651e5] '>Jai Shukla</span> </h1>
 
-    <p className='py-4 text-textColor max-w-[70%] m-auto'>
-Hey there! I'm a fresh graduate super excited about web development, both the front and back ends. I'm all about diving into cool technologies like Next.js and React.js, and I'm totally hooked on TypeScript. Now, I'm on a mission to amp up my skills and explore the fascinating world of decentralized tech. Can't wait to see where this journey takes me! 🚀 #TechEnthusiast #WebDevPassion</p>
+          <h1 className='py-25 uppercase text-[200%] text-textColor'> A Full-Stack Web Developer</h1>
 
-    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <p className='py-4 text-textColor max-w-[70%] m-auto '>
+            Hey there! I'm a fresh graduate super excited about web development, both the front and back ends. I'm all about diving into cool technologies like Next.js and React.js, and I'm totally hooked on TypeScript. Now, I'm on a mission to amp up my skills and explore the fascinating world of decentralized tech. Can't wait to see where this journey takes me! 🚀 #TechEnthusiast #WebDevPassion</p>
 
-    <a
-          href="https://www.linkedin.com/in/jai-shukla-56041016a/"
-          target="_blank"
-          rel="noreferrer"
-        >
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
 
-      <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
-        <RiLinkedinFill className="text-textColor"/>
+            <a
+              href="https://www.linkedin.com/in/jai-shukla-56041016a/"
+              target="_blank"
+              rel="noreferrer"
+            >
+
+              <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <RiLinkedinFill className="text-textColor" />
+              </div>
+            </a>
+
+
+            <a
+              href="https://github.com/lonewolf-hub"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiFillGithub className="text-textColor" />
+              </div>
+            </a>
+            <a
+              href="https://twitter.com/JaiShukla9128"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineTwitter className="text-textColor" />
+              </div>
+            </a>
+            <a
+              href="mailto: iamshuklajai@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail className="text-textColor" />
+              </div>
+            </a>
+
+          </div>
+          <div className='my-4'>
+            <a href='/path/to/your/resume.pdf' download='Jai_Shukla_Resume.pdf'>
+              <button className='bg-[#5651e5] text-white py-2 px-6 rounded-full hover:bg-[#463eca] transition duration-300'>
+                Download Resume
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
-      </a>
-
-
-      <a
-          href="https://github.com/lonewolf-hub"
-          target="_blank"
-          rel="noreferrer"
-        >
-      <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
-      <AiFillGithub className="text-textColor"/>
-      </div>
-      </a>
-      <a
-          href="https://twitter.com/JaiShukla9128"
-          target="_blank"
-          rel="noreferrer"
-        >
-      <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
-      <AiOutlineTwitter className="text-textColor"/>
-      </div>
-      </a>
-      <a
-          href="mailto: iamshuklajai@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-      <div className='rounded-full shadow-lg shadow-gray-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
-      <AiOutlineMail className="text-textColor"/>
-      </div>
-      </a>
-
     </div>
-      </div>
-    </div>
-</div>
   )
 }
 
--- src/app/core/components/skills/SkillItems.tsx
+++ src/app/core/components/skills/SkillItems.tsx
@@ -1,4 +1,4 @@
-// SkillItem.tsx
+
 import React from 'react';
 import Image from 'next/image';
 
