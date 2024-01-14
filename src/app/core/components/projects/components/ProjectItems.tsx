4b426eceb6c0bbf01c25245c78d541e179118c25
Jai Shukla <iamshuklajai@gmail.com>
2024-01-15 02:06:33 +0530

    minor change

--- src/app/core/components/projects/components/ProjectItems.tsx
+++ src/app/core/components/projects/components/ProjectItems.tsx
@@ -6,17 +6,17 @@ interface ProjectItemsProps {
   title: string;
   backgroundImg: string;
   projectUrl: string;
-  Stack: string;
+  stack: string;
 }
 
-const ProjectItems: React.FC<ProjectItemsProps> = ({ title, backgroundImg, projectUrl, Stack }) => {
+const ProjectItems: React.FC<ProjectItemsProps> = ({ title, backgroundImg, projectUrl, stack }) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-[#59fff7] to-[#0f73d8]'>
       <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' width={564} height={310} layout="responsive" objectFit="cover"/>
 
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt-2 text-white text-center'>{Stack}</p>
+        <p className='pb-4 pt-2 text-white text-center'>{stack}</p>
         <Link href={projectUrl}>
           <p className='text-center py-3 px-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>View Details</p>
         </Link>
