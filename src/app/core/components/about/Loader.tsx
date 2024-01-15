a7c94aab22f518548e21b049191b92baf1e13ef9
Jai Shukla <iamshuklajai@gmail.com>
2024-01-15 13:40:00 +0530

    minor ui changes

---
+++ src/app/core/components/about/Loader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FadeLoader } from 'react-spinners';
+
+interface LoaderProps {
+  loading: boolean;
+  message?: string;
+}
+
+const LoaderComponent: React.FC<LoaderProps> = ({ loading, message }) => {
+  return (
+    <div className='flex flex-col py-14'>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <FadeLoader color="#EE9B8D" loading={loading}/>
+        {loading && message && <p className="loader-message">{message}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default LoaderComponent;
