d91f576dcda10273bc698faa8692615fc39cf1c4
Jai Shukla <iamshuklajai@gmail.com>
2024-02-09 10:59:54 +0530

    darkmode fixed

--- src/app/core/components/darkmode/DarkMode.tsx
+++ src/app/core/components/darkmode/DarkMode.tsx
@@ -2,42 +2,25 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const DarkMode = () => {
-  const setDarkmode = () => {
-    document.querySelector('body')!.setAttribute('data-theme', 'dark');
-    if (typeof localStorage !== 'undefined') {
-      localStorage.setItem('selectedTheme', 'dark');
-    }
+  const [selectedTheme, setSelectedTheme] = useState<'light' | 'dark'>(
+    () => (typeof localStorage !== 'undefined' ? (localStorage.getItem('selectedTheme') as 'light' | 'dark') || 'light' : 'light')
+  );
+
+  const setTheme = (theme: 'light' | 'dark') => {
+    document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('selectedTheme', theme);
   };
-
-  const setLightmode = () => {
-    document.querySelector('body')!.setAttribute('data-theme', 'light');
-    if (typeof localStorage !== 'undefined') {
-      localStorage.setItem('selectedTheme', 'light');
-    }
-  };
-
-  const [selectedTheme, setSelectedTheme] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (typeof localStorage !== 'undefined') {
-      setSelectedTheme(localStorage.getItem('selectedTheme'));
-    }
-  }, []);
-
-  useEffect(() => {
-    if (selectedTheme === 'dark') {
-      setDarkmode();
-    } else {
-      setLightmode();
-    }
-  }, [selectedTheme]);
 
   const toggleTheme = () => {
     setSelectedTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   };
 
+  useEffect(() => {
+    setTheme(selectedTheme);
+  }, [selectedTheme]);
+
   return (
-    <div className="relative flex  space-x-2">
+    <div className="relative flex space-x-2">
       <label htmlFor="darkmode-toggle" className="cursor-pointer">
         <input
           type="checkbox"
@@ -46,8 +29,7 @@ const DarkMode = () => {
           checked={selectedTheme === 'dark'}
           className="hidden dark_mode_input"
         />
-        <div className="relative w-14 h-6rounded-full p-1 transition duration-300 ease-in-out flex items-center shadow-md dark_mode_label">
-       
+        <div className="relative w-14 h-6 rounded-full p-1 transition duration-300 ease-in-out flex items-center shadow-md dark_mode_label">
           <div
             className={`absolute right-0 top-1/2 transform -translate-y-1/2 w-7 h-6 rounded-full transition-transform ${
               selectedTheme === 'dark' ? 'opacity-0' : 'opacity-100'
